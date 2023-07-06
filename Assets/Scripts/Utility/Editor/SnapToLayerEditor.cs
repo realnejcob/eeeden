8f35333d6fea472ee6bafff91ee06febc287aec1
nejcob <jacobhvidamstrup@gmail.com>
2023-07-06 12:25:10 +0200

    Added SnapToLayer utility script.

---
+++ Assets/Scripts/Utility/Editor/SnapToLayerEditor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SnapToLayer))]
+public class SnapToLayerEditor : Editor {
+    public override void OnInspectorGUI() { 
+        DrawDefaultInspector();
+        if (GUILayout.Button("Snap!")) {
+            var monoBehaviour = (MonoBehaviour)target;
+            var snapToLayer = monoBehaviour.GetComponent<SnapToLayer>();
+            snapToLayer.Snap();
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/SnapToLayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SnapToLayer : MonoBehaviour {
+    [SerializeField] private LayerMask layer;
+    [SerializeField] private float offset = 0;
+
+    public void Snap() {
+        Ray ray = new Ray(transform.position, -transform.up);
+        if (Physics.Raycast(ray, out var hit, 500, layer)) {
+            if (hit.collider.gameObject != null) {
+                var point = hit.point;
+                transform.position = new Vector3(transform.position.x, point.y + offset, transform.position.z);
+                return;
+            }
+
+            Debug.LogWarning("Can't snap!");
+        }
+    }
+}
