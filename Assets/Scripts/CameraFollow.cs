a9f27beef30a9eed290a5daeab280fb3935d983b
nejcob <jacobhvidamstrup@gmail.com>
2023-08-11 12:21:21 +0200

    Variant of player prefab with new model. Zoom out on L2

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,13 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private Camera cam;
+    private float desiredCamSize = 6;
+    private float defaultCamSize = 6;
+    private float zoomOutCamSize = 10;
+    private float currentCamSize;
+
+    private float camSizeZoomTime = 0.25f;
+    private float camSizeVel;
+
+
     public Transform target;
     private Transform secondaryTarget;
     public Vector3 offset = new Vector3(0, 2, -10);
     public float smoothTime = 0.25f;
     Vector3 currentVelocity;
+
+    private void Start() {
+        currentCamSize = defaultCamSize;
+        desiredCamSize = currentCamSize;
+    }
+
+    private void Update() {
+        CheckForInput();
+        SetCamSize();
+    }
+
+    private void CheckForInput() {
+        if (InputManager.Instance.GetGamepad().leftTrigger.value > 0.1f) {
+            desiredCamSize = zoomOutCamSize;
+        } else {
+            desiredCamSize = defaultCamSize;
+        }
+    }
+
+    private void SetCamSize() {
+        currentCamSize = Mathf.SmoothDamp(currentCamSize, desiredCamSize, ref camSizeVel, camSizeZoomTime);
+        cam.orthographicSize = currentCamSize;
+    }
 
     private void FixedUpdate() {
         var targetPosition = target.position;
